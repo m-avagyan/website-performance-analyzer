f4a74c9742ba339c836459bffd668b470caad1e4
m-avagyan <martikavagyan1@gmail.com>
2024-04-27 23:37:30 +0400

    add prettier configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    useTabs: false,
+    tabWidth: 4,
+    bracketSpacing: true,
+    printWidth: 120,
+    arrowParens: 'always',
+};
